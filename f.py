ee3ece364d3439dbe51984d03b8361f3edb7c94f
Dex <128259345+Mr3nter9@users.noreply.github.com>
2024-08-04 18:00:36 +0300

    Update f.py

--- f.py
+++ f.py
@@ -1,17 +1,17 @@
-def find_line_with_word(file_path, target_word):
+def find_line_number_with_word(file_path, target_word):
     with open(file_path, 'r', encoding='utf-8') as file:
-        for line in file:
+        for line_number, line in enumerate(file, start=1):
             if target_word in line:
-                return line.strip()  # ترجع السطر وتزيل الفراغات الزائدة
+                return line_number
     return None
 
 # مثال على الاستخدام
 file_path = 'd.txt'  # استبدل هذا بمسار الملف الخاص بك
 target_word = 'Qamar242'  # استبدل هذا بالكلمة التي تبحث عنها
 
-result = find_line_with_word(file_path, target_word)
+result = find_line_number_with_word(file_path, target_word)
 
 if result:
-    print(f'السطر الذي يحتوي على الكلمة هو: {result}')
+    print(f'رقم السطر الذي يحتوي على الكلمة هو: {result}')
 else:
     print('لم يتم العثور على الكلمة في الملف.')
